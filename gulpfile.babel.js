45b229302b56357d25344bf17b782097a927f0e1
Marc Hibbins <marchibbins@gmail.com>
2016-09-13 10:52:14 +0100

    Move gulpfile paths to object

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,13 +5,32 @@ import connect from 'gulp-connect';
 import eslint from 'gulp-eslint';
 import gutil from 'gulp-util';
 import rename from 'gulp-rename';
+import path from 'path';
 import sequence from 'run-sequence';
 import source from 'vinyl-source-stream';
 import uglify from 'gulp-uglify';
 
+const dir = {
+        source: './src',
+        build: './dist'
+    },
+    filenames = {
+        js: {
+            app: 'app.js',
+            bundle: 'bundle.js'
+        }
+    },
+    paths = {
+        js: {
+            app: path.join(dir.source, filenames.js.app),
+            bundle: path.join(dir.build, filenames.js.bundle),
+            source: path.join(dir.source, '**/*.js')
+        }
+    };
+
 gulp.task('bundle', () => {
-    gutil.log(gutil.colors.green('Bundling \'./src/app.js\''));
-    return browserify('./src/app.js')
+    gutil.log(gutil.colors.green(`Bundling '${paths.js.app}'`));
+    return browserify(paths.js.app)
         .transform(babelify, {
             presets: ['es2015', 'react']
         })
@@ -20,16 +39,16 @@ gulp.task('bundle', () => {
             gutil.log(gutil.colors.red(err.toString()));
             this.emit('end');
         })
-        .pipe(source('bundle.js'))
-        .pipe(gulp.dest('./dist'));
+        .pipe(source(filenames.js.bundle))
+        .pipe(gulp.dest(dir.build));
 });
 
 gulp.task('compress', () => {
-    gutil.log(gutil.colors.green('Compressing \'./dist/bundle.js\''));
-    return gulp.src('./dist/bundle.js')
+    gutil.log(gutil.colors.green(`Compressing '${paths.js.bundle}'`));
+    return gulp.src(paths.js.bundle)
         .pipe(uglify())
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('./dist'));
+        .pipe(gulp.dest(dir.build));
 });
 
 gulp.task('js', (callback) => {
@@ -39,8 +58,8 @@ gulp.task('js', (callback) => {
 });
 
 gulp.task('lint', () => {
-    gutil.log(gutil.colors.green('Linting \'./src/**/*.js\''));
-    return gulp.src('./src/**/*.js')
+    gutil.log(gutil.colors.green(`Linting '${paths.js.source}'`));
+    return gulp.src(paths.js.source)
         .pipe(eslint())
         .pipe(eslint.format());
 });
@@ -50,8 +69,8 @@ gulp.task('serve', () => {
 });
 
 gulp.task('watch', () => {
-    gutil.log(gutil.colors.green('Watching \'./src/**/*.js\' for changes'));
-    gulp.watch('./src/**/*.js', ['js']);
+    gutil.log(gutil.colors.green(`Watching '${paths.js.source}' for changes`));
+    gulp.watch(paths.js.source, ['js']);
 });
 
 gulp.task('default', ['js']);
