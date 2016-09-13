e2dad5d58a34bda33315fcae5ad152ed3ab4afaf
Marc Hibbins <marchibbins@gmail.com>
2016-09-13 09:28:57 +0100

    Introduce uglify pre-production, see todo

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,9 +4,13 @@ import browserify from 'browserify';
 import connect from 'gulp-connect';
 import eslint from 'gulp-eslint';
 import gutil from 'gulp-util';
+import rename from 'gulp-rename';
+import sequence from 'run-sequence';
 import source from 'vinyl-source-stream';
+import uglify from 'gulp-uglify';
 
-gulp.task('js', ['lint'], () => {
+gulp.task('bundle', () => {
+    gutil.log(gutil.colors.green('Bundling \'./src/app.js\''));
     return browserify('./src/app.js')
         .transform(babelify, {
             presets: ['es2015', 'react']
@@ -20,7 +24,22 @@ gulp.task('js', ['lint'], () => {
         .pipe(gulp.dest('./dist'));
 });
 
+gulp.task('compress', () => {
+    gutil.log(gutil.colors.green('Compressing \'./dist/bundle.js\''));
+    return gulp.src('./dist/bundle.js')
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('js', (callback) => {
+    // TODO: This produces a minified development version of React,
+    // set process.env.NODE_ENV = 'production' before bundle.
+    sequence('lint', 'bundle', 'compress', callback);
+});
+
 gulp.task('lint', () => {
+    gutil.log(gutil.colors.green('Linting \'./src/**/*.js\''));
     return gulp.src('./src/**/*.js')
         .pipe(eslint())
         .pipe(eslint.format());
