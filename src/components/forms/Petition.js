68f20b3535c4892f6f6257cbd1119e7a3bff3044
Marc Hibbins <marchibbins@gmail.com>
2016-09-15 14:24:36 +0100

    Correctly control form components

--- src/components/forms/Petition.js
+++ src/components/forms/Petition.js
@@ -11,7 +11,11 @@ class Petition extends React.Component {
 
     constructor (props) {
         super(props);
-        this.state = {};
+        this.state = {
+            firstname: '',
+            lastname: '',
+            email: ''
+        };
         this.validatorTypes = {
             firstname: Joi.string().required().label('First name'),
             lastname: Joi.string().required().label('Last name'),
@@ -24,7 +28,7 @@ class Petition extends React.Component {
     }
 
     handleChange (name, event) {
-        let state = {};
+        let state = this.state;
         state[name] = event.target.value;
         this.setState(state);
     }
@@ -45,18 +49,27 @@ class Petition extends React.Component {
             <form onSubmit={this.handleSubmit.bind(this)} noValidate>
                 <div>
                     <label>First name</label>
-                    <input type="text" onChange={this.handleChange.bind(this, 'firstname')}/>
-                    <span className='help-block'>{this.props.getValidationMessages('firstname')[0]}</span>
+                    <input type="text" value={this.state.firstname}
+                        onChange={this.handleChange.bind(this, 'firstname')}/>
+                    <div className='help-block'>
+                        {this.props.getValidationMessages('firstname')[0]}
+                    </div>
                 </div>
                 <div>
                     <label>Last name</label>
-                    <input type="text" onChange={this.handleChange.bind(this, 'lastname')}/>
-                    <span className='help-block'>{this.props.getValidationMessages('lastname')[0]}</span>
+                    <input type="text" value={this.state.lastname}
+                        onChange={this.handleChange.bind(this, 'lastname')}/>
+                    <div className='help-block'>
+                        {this.props.getValidationMessages('lastname')[0]}
+                    </div>
                 </div>
                 <div>
                     <label>Email</label>
-                    <input type="email" onChange={this.handleChange.bind(this, 'email')}/>
-                    <span className='help-block'>{this.props.getValidationMessages('email')[0]}</span>
+                    <input type="email" value={this.state.email}
+                        onChange={this.handleChange.bind(this, 'email')}/>
+                    <div className='help-block'>
+                        {this.props.getValidationMessages('email')[0]}
+                    </div>
                 </div>
                 <input type="submit" value="Sign our petition"/>
             </form>
