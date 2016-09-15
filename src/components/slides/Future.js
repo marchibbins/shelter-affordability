89c54b53e291c8a423c419e765a0893e7c56f701
Marc Hibbins <marchibbins@gmail.com>
2016-09-15 10:49:01 +0100

    Placeholder form for petition

--- src/components/slides/Future.js
+++ src/components/slides/Future.js
@@ -8,12 +8,26 @@ class Future extends React.Component {
         return Math.abs(diff) + ' ' + (diff > 0 ? 'more' : 'less');
     }
 
+    handleSubmit (event) {
+        event.preventDefault();
+        this.props.gotoNext();
+    }
+
     render () {
         return (
             <article>
                 <h1>We built {this.props.buildsLastYear} homes in the UK last year, {this.getBuildsDiff()} than in {this.props.yob} when you were born.</h1>
-                <h2>Please sign our petition</h2>
-                <button onClick={this.props.gotoNext}>Sign petition</button>
+                <h2>Please sign our petition to demand that Theresa May and her government start building the homes we all urgently need.</h2>
+
+                <form onSubmit={this.handleSubmit.bind(this)}>
+                    <label>First name</label>
+                    <input type="text" name="firstname"/>
+                    <label>Last name</label>
+                    <input type="text" name="lastname"/>
+                    <label>Email</label>
+                    <input type="email" name="email"/>
+                    <input type="submit" value="Sign our petition"/>
+                </form>
             </article>
         );
     }
