6f485a9aae7524f78bc3e426f213bd9529e6cc48
Marc Hibbins <marchibbins@gmail.com>
2016-09-15 13:36:26 +0100

    Add outline for questions form

--- src/components/slides/Questions.js
+++ src/components/slides/Questions.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import QuestionsForm from '../forms/Questions';
+
 export default class Questions extends React.Component {
 
     render () {
         return (
             <article>
                 <h1>Before you go anywhere, can you please quickly answer a few questions?</h1>
+                <h2>This will really help us understand a bit more about our supporters</h2>
+                <QuestionsForm/>
             </article>
         );
     }
