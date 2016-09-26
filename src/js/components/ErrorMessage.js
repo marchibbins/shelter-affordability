3df9249635ae4ef657d654293237b722385d5bc0
Marc Hibbins <marchibbins@gmail.com>
2016-09-26 11:24:05 +0100

    Colour reponse message

--- src/js/components/ErrorMessage.js
+++ src/js/components/ErrorMessage.js
@@ -4,7 +4,7 @@ export default class ErrorMessage extends React.Component {
 
     render () {
         return (
-            <em>ErrorMessage {this.props.message}</em>
+            <em className="message">ErrorMessage {this.props.message}</em>
         );
     }
 
