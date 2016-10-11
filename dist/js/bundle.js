93273ec89d59144719d192dbfbab0ab050a6fa54
Marc Hibbins <marchibbins@gmail.com>
2016-10-11 09:46:04 +0100

    Loop progress without array keys

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -48236,8 +48236,6 @@ var _react2 = _interopRequireDefault(_react);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
-
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
@@ -48261,7 +48259,7 @@ var Progress = function (_React$Component) {
             return _react2.default.createElement(
                 'ol',
                 { className: 'unbulleted slide-progress ' + (this.props.index === this.props.slides.length - 1 ? 'hide' : '') },
-                [].concat(_toConsumableArray(Array(this.props.slides.length).keys())).map(function (i) {
+                this.props.slides.map(function (slide, i) {
                     return _react2.default.createElement('li', { key: i, className: 'slide-progress__dot ' + (i <= _this2.props.index ? 'active' : '') });
                 })
             );
--- src/js/components/Progress.js
+++ src/js/components/Progress.js
@@ -6,7 +6,7 @@ export default class Progress extends React.Component {
         return (
             <ol className={'unbulleted slide-progress '
                     + (this.props.index === this.props.slides.length - 1 ? 'hide' : '')}>
-                {[...Array(this.props.slides.length).keys()].map(i => {
+                {this.props.slides.map((slide, i) => {
                     return (
                         <li key={i} className={'slide-progress__dot ' + (i <= this.props.index ? 'active' : '')}></li>
                     );
